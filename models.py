e64a20f5fa27127d89b45fbb3feda5f6c78df6c9
Dmitry Ikonnikov <ikosha-tam@yandex.ru>
2022-10-20 14:36:48 +0300

    Add column telegram_id in table 'Users', change .gitignore (add alembic.ini) and requirements.txt

--- models.py
+++ models.py
@@ -1,3 +1,4 @@
+from enum import unique
 from sqlalchemy import Column, Integer, String, DateTime, JSON, Boolean
 from db import Base, engine
 
@@ -6,6 +7,7 @@ class User(Base):
     __tablename__ = 'users'
     id = Column(Integer, primary_key=True)
     username = Column(String, unique=True)
+    telegram_id = Column(Integer, unique=True)
     name = Column(String)
     date_time_registration = Column(DateTime)
     last_time_active = Column(DateTime)
